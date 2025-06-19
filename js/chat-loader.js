6855d1e2dfac238bb31401ff34b7d5cea1a59862
katudf <katudf@gmail.com>
2025-06-19 14:33:47 +0900

    新規ボタンをアイコンに変更

--- js/chat-loader.js
+++ js/chat-loader.js
@@ -71,7 +71,7 @@
                         <span id="chat-widget-title">AIに相談</span>
                         <span id="prompt-version-display"></span>
                         <div class="chat-header-buttons">
-                            <button id="chat-widget-new-button">新規</button>
+                            <button id="chat-widget-new-button" aria-label="新しい会話を開始"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417l.3 1.5a.5.5 0 0 1-.411.589A6 6 0 1 1 8 2v1z"/><path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658a.25.25 0 0 1-.41-.192z"/></svg></button>
                             <button id="chat-widget-close-button">×</button>
                         </div>
                     </div>
